b626bca62a3be18dd2e0e2dd0b4d4ac28ea92d4a
Lại Xuân Biển <quyblack6@gmail.com>
2026-03-24 07:17:10 +0700

    Delete QuanLyCuaHangTienLoi directory

--- QuanLyCuaHangTienLoi/QuanLyCuaHangTienLoi/src/main/java/com/mycompany/quanlycuahangtienloi/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.quanlycuahangtienloi;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Khởi động ứng dụng bằng cách gọi ké hàm main của MainApp từ đây
-        MainApp.main(args);
-    }
-}
